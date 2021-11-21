4423c8d6150c9e73bf848d3051df83929ea62edd
ChattadaV <plychattada@yahoo.com>
2021-11-21 15:10:18 -0600

    init

--- project.py
+++ project.py
@@ -1,15 +1,16 @@
 from pylab import *
 import matplotlib.pyplot as plt
 
-#x and y axis from 0 to 100 at 0.1 step increment
+# x and y axis from 0 to 100 at 0.1 step increment
 s, i = meshgrid(arange(0, 100, 0.1), arange(0, 100, 0.1))
 
-r = 1 #transmission rate
-y = 5 #tendency to enter r-state (recover/die, cannot pass on virus)
+r = 1 # transmission rate
+y = 5 # tendency to enter r-state (recover/die, cannot pass on virus)
 
-#derivative equations
+# Derivative equations
 sDot = -r*s*i
 iDot = r*s*i-y*i
 
+# Plot both  normal and derivative equations
 streamplot(s, i, sDot, iDot)
 show()
