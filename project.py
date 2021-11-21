bfcf8237c461c8a3492a5862825453a9bcebf3f9
ChattadaV <plychattada@yahoo.com>
2021-11-21 15:14:21 -0600

    Comments added

--- project.py
+++ project.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 # x and y axis from 0 to 100 at 0.1 step increment
 s, i = meshgrid(arange(0, 100, 0.1), arange(0, 100, 0.1))
 
-r = 1 # transmission rate
-y = 5 # tendency to enter r-state (recover/die, cannot pass on virus)
+r = 1 # Transmission rate
+y = 5 # Tendency to enter r-state (recover/die, cannot pass on virus)
 
 # Derivative equations
 sDot = -r*s*i
