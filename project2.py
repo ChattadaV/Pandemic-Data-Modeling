aa4c4469f2deda8a9a812a0544e3b37dbf445ef7
ChattadaV <plychattada@gmail.com>
2021-12-02 19:28:04 -0600

    Update 12/02/2021

--- project2.py
+++ project2.py
@@ -1,3 +1,5 @@
+# Normal derivative equations with non-constant R
+
 from pylab import *
 import matplotlib.pyplot as plt
 
@@ -8,13 +10,16 @@ k = 30
 b = 0.85
 n = 3.1
 
-r = ( b*(k**n) ) / ( (i**n) + (k**n) )
-y = arange(0, 100, 0.1)
+r = ( b*(k**n) ) / ( (i**n) + (k**n) ) #transmission rate
+y = 10 #tendency to enter r-state (recove/die, cannot pass on virus)
 
 sDot = -r*s*i
 iDot = (r*s*i) - (y*i)
 
 streamplot(s, i, sDot, iDot)
+plt.title("Relationship between Infected and Susceptible among the Population", fontsize = 12)
+plt.xlabel("Number of Susceptible", fontsize = 10)
+plt.ylabel("Number of Infected", fontsize = 10)
 show()
 
 
--- project3.py
+++ project3.py
@@ -1,22 +1,25 @@
-# Normal derivative equations with constant R
+# Different derivative equations with constant R
 
 from pylab import *
 import matplotlib.pyplot as plt
 
-#s and i in meshgrid with range from 0 to 100 at step increment of 0.1
+#s and i in meshgrid with range from -10 to 100 at step increment of 0.1
 s, i = meshgrid(arange(0, 100, 0.1), arange(0, 100, 0.1))
 
 #constant values
 r = 0.02 #transmission rate
 y = 0.2 #tendency to enter r-state (recove/die, cannot pass on virus)
 
+m = 1/75
+n = 1000
+
 #derivative equations (streamline)
-sDot = -r*s*i
-iDot = r*s*i-y*i
+sDot = (((-r * i * s) / n) + m - ((m * s) / n))
+iDot = ((r * s * i) / n) - ((y + m) * (i / n))
 
 #plot
 streamplot(s, i, sDot, iDot)
-plt.title("Relationship between Infected and Susceptible among the Population", fontsize = 12)
+plt.title("Infected-Susceptible among Population with Constant R", fontsize = 12)
 plt.xlabel("Number of Susceptible", fontsize = 10)
 plt.ylabel("Number of Infected", fontsize = 10)
 show()
